ee23157765104fd7f8816f63231b180663a8876a
14237077 <14237077@Terra02.Fortis.local>
2024-06-11 11:05:26 +0200

    Beispiel

---
+++ src/main/java/com/mycompany/beispiel/Beispiel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.beispiel;
+
+/**
+ *
+ * @author 14237077
+ */
+public class Beispiel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
